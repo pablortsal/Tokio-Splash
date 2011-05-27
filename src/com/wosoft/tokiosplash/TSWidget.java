1ebafd9a5194b7c0686168ae669e47f032718ded
Guillermo Orellana Ruiz <wiyarmir@gmail.com>
2011-05-27 04:13:02 +0200

    Initial commit Signed-off-by: Guillermo Orellana Ruiz <wiyarmir@gmail.com>

---
+++ src/com/wosoft/tokiosplash/TSWidget.java
@@ -0,0 +1,16 @@
+package com.wosoft.tokiosplash;
+
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+ 
+public class TSWidget extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context,
+                 AppWidgetManager appWidgetManager,
+                 int[] appWidgetIds) {
+        //Actualizar el widget
+        //...
+    }
+}
